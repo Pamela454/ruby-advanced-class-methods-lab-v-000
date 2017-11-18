4a4558b4c22f4ef30b8251c9386b978043cb4076
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 16:47:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,10 @@ class Song
   end
 
   def Song.create_from_filename(filename)
-    
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
   end
 
   def save
