eab1dd75888ec3e31a0e039bb3a03928eba6c239
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 15:42:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,29 @@
+#code that relies on abstraction is more maintainable and extendable over time
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
   def self.all
     @@all
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    @@all << song
+    song
   end
 
   def save
